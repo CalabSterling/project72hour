06fa5c7721ae9e618f0edea5711253c3856fc36e
Telma Chapa <telchapa@gmail.com>
2021-08-26 21:42:11 -0400

    added ticketmaster

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React from 'react';
+import TktMaster from './TktMaster';
 
 function App() {
+
   return (
     <div>
       <h1>72 Hour Project</h1>
+      <TktMaster />
     </div>
   );
 }
---
+++ src/TktMaster.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+
+const TktMaster = (props) => {
+    // const [latLong, setlatLong] = useState('');
+
+    // useEffect(() => {
+    //     console.log('location found');
+    // })
+    // }
+    
+    const handleClick = async() => {
+        const url = `https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=QInF0b7Aa2nMHrALmSHEEq4uwNwr5un8`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data._embedded.events);
+        
+    }
+
+
+
+    return (
+         <div>
+         <p>Hello from Ticketmaster</p>
+
+         <button onClick={handleClick}>Click Me</button>
+    </div> );
+
+
+}
+ 
+export default TktMaster;
