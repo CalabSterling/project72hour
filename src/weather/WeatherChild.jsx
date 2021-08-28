71599cb88613142d183bca6f74ad898b7cf838a8
Kara Smith <MsKaraBSmith@gmail.com>
2021-08-28 09:20:59 -0400

    fahrenheit works!

--- src/weather/WeatherChild.jsx
+++ src/weather/WeatherChild.jsx
@@ -13,13 +13,14 @@ const WeatherChild = (props) => {
         const feelsLike = props.temperaturetoday.feels_like;
         let temperatureFahrenheit = temperature * 1.8 + 32;
         let feelsLikeFahrenheit = feelsLike * 1.8 + 32;
+        setNewTemperature (!newTemperature)
         setGetFahrenheit(temperatureFahrenheit);
         console.log(feelsLikeFahrenheit);
     }
     
-    const fahrenheit = () => {
-      setNewTemperature (!newTemperature)
-    }
+    // const fahrenheit = () => {
+    //   setNewTemperature (!newTemperature)
+    // }
 
     const changeTemp = () => {
       return <>{getFahrenheit}</>
@@ -28,12 +29,11 @@ const WeatherChild = (props) => {
     return ( 
         <div>
         
-          <button onClick={fahrenheit}>Change Temperature</button>
-          <CardBody id="temp">{newTemperature ? "Fahrenheit" : "Celsius"} {newTemperature ? getFahrenheit : ""}°
-            <CardTitle  className="text-primary">Current Temp: {temp}°</CardTitle>
-            <CardSubtitle className="text-primary">Feels Like: {feels_like}°</CardSubtitle>
+          <button onClick={convertTemp}>Change Temperature</button>
+          <CardBody id="temp">{newTemperature ? "Fahrenheit" : "Celsius"}
+            <CardTitle  className="text-primary">Current Temp: {newTemperature ? getFahrenheit : temp }°</CardTitle>
+            <CardSubtitle className="text-primary">Feels Like: {newTemperature ? getFahrenheit : feels_like}°</CardSubtitle>
             <CardText className="text-primary">Humidty is {humidity}.</CardText>
-            <input type="checkbox" value="true" onChange={convertTemp}/>
           </CardBody>
         
       </div> 
